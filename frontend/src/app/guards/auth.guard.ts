d42df315fa4b193ece6c84f4ae547e6d9f54d20b
kecskecske6 <kecskecske6@gmail.com>
2022-06-25 12:43:27 +0200

    This has to fix it!

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AppComponent } from '../app.component';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private appComponent: AppComponent) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.appComponent.user) return true;
-    return false;
+    return new Observable<boolean>(obs => {
+      this.userService.getUser().subscribe({
+        next: result => {
+          if (result.hasOwnProperty('userId')) obs.next(true);
+          else {
+            this.router.navigate(['/']);
+            obs.next(false);
+          }
+        },
+        error: err => {
+          console.log(err);
+          this.router.navigate(['/']);
+          obs.next(false)
+        }
+      });
+    });
   }
 
 }
