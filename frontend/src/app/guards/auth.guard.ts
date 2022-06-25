ba7a71a6348d3262bb095a8fec0b5f08302c267e
kecskecske6 <kecskecske6@gmail.com>
2022-06-25 10:25:13 +0200

    Probably auth guard fix

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { NewNewsComponent } from '../components/new-news/new-news.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +12,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (route.component == NewNewsComponent) {
-      if (localStorage.getItem('tag') == 'kecskécske6#4166') return true;
-      return false;
-    }
     if (localStorage.getItem('tag')) return true;
     return false;
   }
